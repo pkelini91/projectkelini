f3119780945bb66909d60cf41d5a8fd4cda271ee
peter <kelinipeter@gmail.com>
2018-06-19 10:15:17 -0400

    laravel setup for new project new header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.default')
 @section('content')
-   <h1>Hello World! Welcome to my Homepage</h1>
 
+
+
+   <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">  Welcome to my homepage!.</p>
+      <hr class="my-4">
+      <p>  </p>
+      <p class="lead">
+         <a class="btn btn-primary btn-lg" href="http://www.yahoo.com" role="button">Yahoo.com!</a>
+      </p>
 
    <style>
       table {
@@ -23,57 +32,10 @@
    </head>
    <body>
 
-   <h2>HTML Table</h2>
 
-   <table>
-      <tr>
-         <th>Company</th>
-         <th>Contact</th>
-         <th>Country</th>
-         <th>Phone</th>
-         <th>Sex</th>
-      </tr>
-      <tr>
-         <td>Alfreds Futterkiste</td>
-         <td>Maria Anders</td>
-         <td>Germany</td>
-         <td>732-277-6790</td>
-         <td>Female</td>
-      </tr>
-      <tr>
-         <td>Centro comercial Moctezuma</td>
-         <td>Francisco Chang</td>
-         <td>Mexico</td>
-         <td>732-277-6790</td>
-         <td>Female</td>
-      </tr>
-      <tr>
-         <td>Ernst Handel</td>
-         <td>Roland Mendel</td>
-         <td>Austria</td>
-         <td>732-277-6790</td>
-         <td>male</td>
-      </tr>
-      <tr>
-         <td>Island Trading</td>
-         <td>Helen Bennett</td>
-         <td>UK</td>
 
-         <td>732-277-6790</td>
-         <td>Female</td>
-      </tr>
-      <tr>
-         <td>Laughing Bacchus Winecellars</td>
-         <td>Yoshi Tannamuri</td>
-         <td>Canada</td>
 
-         <td>732-277-6790</td>
-         <td>Male</td>
-      </tr>
 
-   </table>
-
-   <a href="http://www.yahoo.com">here</a> to go to yahoo.
 
 
    <img src="flag.jpj" width="100" height="100">
